6987253108729c04d160b566b3edf500100b60ef
AndreyChuck <a.chucklanov@yandex.ru>
2023-02-21 21:42:49 +0400

    Добавлен проект ConsoleApp3.2.8

---
+++ ConsoleApp3.2.8/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp3._2._8
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            const string MyName = "Jane";
+
+            Console.WriteLine(MyName);
+
+            Console.WriteLine("\t Здравствуй Мир!");
+            Console.WriteLine("\t Мне 27 лет");
+            Console.WriteLine("\t My name is \n {0}", MyName);
+            Console.WriteLine("\u0040");
+            Console.WriteLine("\u0023");
+            Console.ReadKey();
+
+        }
+    }
+}
